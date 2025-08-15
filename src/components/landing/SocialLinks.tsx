d9a01db401de39bde6fad0c1e60274871c4c3211
Denilson Bezerra <denilsonbezerra.10@hotmail.com>
2025-08-15 17:24:23 -0300

    style: icones branco

--- src/components/landing/SocialLinks.tsx
+++ src/components/landing/SocialLinks.tsx
@@ -7,7 +7,7 @@ interface SocialButtonProps {
 
 export default function SocialLinks({ href, icon } : SocialButtonProps) {
     return (
-        <Link to={href} target="_blank" className="bg-secondary p-[8px] rounded-full transition-all duration-300 ease-in-out hover:opacity-80 active:scale-95 text-foreground">
+        <Link to={href} target="_blank" className="bg-secondary p-[8px] rounded-full transition-all duration-300 ease-in-out hover:opacity-80 active:scale-95 text-white">
             {icon}
         </Link>
     )
